9c14e37f38219c95b0d3fb59b1286c6ecb002186
krojac01 <krojac01@gmail.com>
2020-01-14 20:34:22 -0500

    First commit for this project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+    private Vector3 targetPosition;
+    [SerializeField]
+    [Range(0,1f)]
+    private float moveSpeed;
+
+    private int waypointIndex;
+
+
+    void Start()
+    {
+        targetPosition = waypoints[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.5f * moveSpeed);
+        if(Vector3.Distance(transform.position, targetPosition) < .25f)
+        {
+            if(waypointIndex >= waypoints.Length -1)
+            {
+                waypointIndex = 0;
+            }
+            else
+            {
+                waypointIndex++;
+            }
+            targetPosition = waypoints[waypointIndex].position;
+        }
+    }
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+
+    private int requiredCoins;
+    private Game game;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = GameObject.FindObjectOfType<Game>();
+        requiredCoins = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    public void CheckForCompletion(int coinCount)
+    {
+        if(coinCount >= requiredCoins)
+        {
+            game.LoadNextLevel();
+        }
+        else
+        {
+            Debug.Log("You do not have enough coins");
+        }
+    }
+}
